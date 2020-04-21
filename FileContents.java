df6d84b212dd014cb64456154a3411dcea37787e
Bret A. Van Hof <bretvh@hotmail.com>
2020-04-21 04:54:48 -0400

    Fix indentation

--- FileContents.java
+++ FileContents.java
@@ -4,12 +4,12 @@ import java.util.*;
 public class FileContents implements Serializable { 
  private byte[] contents; // file contents 
  public FileContents( byte[] contents ) { 
- this.contents = contents; 
+     this.contents = contents; 
  } 
  public void print( ) throws IOException { 
- System.out.println( "FileContents = " + contents ); 
+     System.out.println( "FileContents = " + contents ); 
  } 
  public byte[] get( ) { 
- return contents; 
+     return contents; 
  }
 }
